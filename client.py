711d3b971ffbf4bf7d600d33b512b1a7ad30fc74
Vunguye1 <s364540@oslomet.no>
2023-02-05 22:57:56 +0100

    maybe

--- client.py
+++ client.py
@@ -1,26 +1,28 @@
 from socket import *
 
 
-serverName = '127.0.0.1'
-serverPort = 2609 # use a nice high number (4 digits) if we r playing around
+def request_to_server(server_host, server_port, path):
+        
 
-# create an INET, STREAMing socket
-client_socket = socket(AF_INET, SOCK_STREAM)
+        # create an INET, STREAMing socket
+        client_socket = socket(AF_INET, SOCK_STREAM)
 
-# bind the socket to localhost, and a well-known port
-client_socket.connect((serverName,serverPort))
-        # https://stackoverflow.com/questions/7334199/getaddrinfo-failed-what-does-that-mean
+        # bind the socket to localhost, and a well-known port
+        client_socket.connect((server_host,server_port))
+                # https://stackoverflow.com/questions/7334199/getaddrinfo-failed-what-does-that-mean
 
-# input from client
-sentence = input("Input lowercase sentence: ")
+        # input from client
+        request = f"GET {path} HTTP/1.1\n\n"
 
+        # become a server socket
+        client_socket.send(request.encode()) # send request til server
+        response = ''
 
+        while True:
+                message_from_server = client_socket.recv(1024) # get back message from server
+                if not message_from_server:
+                        break
+                response += message_from_server
 
-# become a server socket
-client_socket.send(sentence.encode()) # send input til server
-modified_Sentence = client_socket.recv(1024) # get back message from server
-
-
-
-print("From Server:", modified_Sentence.decode()) # print message from server to terminal
-client_socket.close()
+        print("From Server: " + response) # print message from server to terminal
+        client_socket.close()
