3e403a39880999b04462b1531c511d9bbc155156
Vunguye1 <s364540@oslomet.no>
2023-02-11 16:29:43 +0100

    working on task 3

--- client_task2.py
+++ client_task2.py
@@ -2,24 +2,22 @@ from socket import *
 import sys
 
 
-def request_to_server(host, port, fil_name):  # function with 3 arguments
+def request_to_server(host, port, fil_name):  # function with 3 arguments host, port and file name
     # create an INET, STREAMing socket
     client_socket = socket(AF_INET, SOCK_STREAM)
 
     # connect to the server
     client_socket.connect((host, port))
 
-    # prepare request
-
     # send request til server
     client_socket.send(fil_name.encode())
     message_from_server = client_socket.recv(1024)  # get the message from server
-    print("From server: ", message_from_server.decode())  # print it out
-    client_socket.close()
+    print("From server: ", message_from_server.decode())  # print the server message out
+    client_socket.close()  # close the socket
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # this function is used for running the python file.
     server_host = sys.argv[1]  # first argument
     server_port = int(sys.argv[2])  # second argument
     filename = sys.argv[3]  # third argument
-    request_to_server(server_host, server_port, filename)
+    request_to_server(server_host, server_port, filename)  # call the function above
---
+++ server_task3.py
@@ -0,0 +1,44 @@
+from socket import *
+import _thread as thread
+
+
+def handle_each_request(connection):
+    while True:
+        path = ""
+        response_from_server = ''
+        request = connection.recv(1024).decode()  # receive request from client/web browser.
+
+        # check if this is the request from web browser or our custom client
+        if "GET" in request:  # request from web browser
+            request_lines = request.split("\n")  # there are many lines from web server request.
+            request_line = request_lines[0].split(" ")  # We only take the first line which is "GET /filename HTTP/1.1"
+            path = request_line[1]  # get the path
+        else:  # request from client
+            path = ("/" + request)  # Path to the file in system
+        try:  # If the file is found
+            read = open(path[1:], 'r')
+            response_from_server = 'HTTP/1.1 200 OK\n\n' + read.read()  # get the contents from HTML-file
+        except FileNotFoundError:  # If the file is not found
+            response_from_server = 'HTTP/1.1 404 Not Found'
+        connection.send(response_from_server.encode())  # send data back from server to client
+        # over the connection
+    connection.close()
+
+
+# define which client you will connect to
+server_Port = 2609
+server_Host = 'localhost'
+
+server_Socket = socket(AF_INET, SOCK_STREAM)  # create a socket
+server_Socket.bind((server_Host, server_Port))  # bind the address of connection from client to the socket
+server_Socket.listen(1)  # activate listening on the socket
+
+print("Vu-server is ready to receive ur message")
+
+while True:
+    connection_Socket, addr = server_Socket.accept()  # accept connections from the outside.
+    # Create a new socket for that connection on return
+    # -> server can use connection_Socket to communicate with client/web browser
+    connection_Socket.send(f"Server connected by the following address: {addr}".encode())
+    thread.start_new_thread(handle_each_request, (connection_Socket,))
+server_Socket.close()
