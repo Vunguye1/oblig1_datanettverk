480857c13190c8345ac0b7602db8af2167070c5e
Vunguye1 <s364540@oslomet.no>
2023-02-11 15:15:12 +0100

    done task 1

--- client.py
+++ client.py
@@ -2,32 +2,29 @@ from socket import *
 
 
 def request_to_server(server_host, server_port, path):
-        
+    # create an INET, STREAMing socket
+    client_socket = socket(AF_INET, SOCK_STREAM)
 
-        # create an INET, STREAMing socket
-        client_socket = socket(AF_INET, SOCK_STREAM)
+    # connect to the server
+    client_socket.connect((server_host, server_port))
 
-        # connect to the server
-        client_socket.connect((server_host,server_port))
-                
+    # prepare request
+    request = f"GET {path} HTTP/1.1\n\n"
 
-        # prepare request
-        request = f"GET {path} HTTP/1.1\n\n"
+    # send request til server
+    client_socket.send(request.encode())
+    response = ''
 
-        # send request til server
-        client_socket.send(request.encode()) 
-        response = ''
+    while True:
+        message_from_server = client_socket.recv(1024)  # get back and read data from server
+        if not message_from_server:
+            break
+        response += message_from_server  # add data from server to our message-variable
 
-        while True:
-                message_from_server = client_socket.recv(1024) # get back and read data from server
-                if not message_from_server:
-                        break
-                response += message_from_server # add data from server to our message-variable
+    print("From Server: " + response)  # print the response from server to terminal
 
-        print("From Server: " + response) # print the response from server to terminal
+    # close the socket
+    client_socket.close()
 
-        # close the socket
-        client_socket.close()
-
-
-# viet funksjon kall på The following is an input command format to run the client. client.py server host server port filename
+# viet funksjon kall på The following is an input command format to run the client.
+# client.py server host server port filename
--- server_testing.py
+++ server_testing.py
@@ -5,16 +5,25 @@ server_Port = 2609
 server_Host = 'localhost'
 
 server_Socket = socket(AF_INET, SOCK_STREAM)  # create a socket
-server_Socket.bind((server_Host, server_Port))  # bind the adress of connection from client to the socket
-server_Socket.listen(1)  # activate listening on the oscket
+server_Socket.bind((server_Host, server_Port))  # bind the address of connection from client to the socket
+server_Socket.listen(1)  # activate listening on the socket
 
 print("Vu-server is ready to receive ur message")
 
-connection_Socket, addr = server_Socket.accept()  # accept connections from the outside. Create a new socket for that connection on return
-# -> server can use connection_Socket to communicate with client
+connection_Socket, addr = server_Socket.accept()  # accept connections from the outside.
+# Create a new socket for that connection on return
+# -> server can use connection_Socket to communicate with client/web browser
 
-request = connection_Socket.recv(1024).decode('utf-8')  # receive request from client.
-path = ("/" + request)  # Path to the file in system
+request = connection_Socket.recv(1024).decode('utf-8')  # receive request from client/web browser.
+
+path = ""
+# check if this is the request from web browser or our custom client
+if "GET" in request:  # request from web browser
+    request_lines = request.split("\n")  # there are many lines from web server request.
+    request_line = request_lines[0].split(" ")  # We only take the first line which is "GET /filename HTTP/1.1"
+    path = request_line[1]  # get the path
+else:  # request from client
+    path = ("/" + request)  # Path to the file in system
 
 response_from_server = ''
 try:  # If the file is found
@@ -22,6 +31,7 @@ try:  # If the file is found
     response_from_server = 'HTTP/1.1 200 OK\n\n' + read.read()  # get the contents from HTML-file
 except FileNotFoundError:  # If the file is not found
     response_from_server = 'HTTP/1.1 404 Not Found'
+    pass
 
 connection_Socket.send(response_from_server.encode())  # send data back from server to client over the connection
 
