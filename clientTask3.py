0f4c45894d958bf2ba5aef5bdafa7292a85fca61
Vunguye1 <s364540@oslomet.no>
2023-02-11 17:49:53 +0100

    done task 3

--- clientTask3.py
+++ clientTask3.py
@@ -1,33 +1,25 @@
 from socket import *
 import sys
 
+server_host = "127.0.0.1"  # first argument
+server_port = 2609  # second argument
+# create an INET, STREAMing socket
+client_socket = socket(AF_INET, SOCK_STREAM)
 
-def request_to_server(host, port, fil_name):  # function with 3 arguments
-    # create an INET, STREAMing socket
-    client_socket = socket(AF_INET, SOCK_STREAM)
+# connect to the server
+try:
+    client_socket.connect((server_host, server_port))
+except:
+    print("Can not connect")
+    sys.exit()
 
-    # connect to the server
-    try:
-        client_socket.connect((host, port))
-    except:
-        print("Can not connect")
-        sys.exit()
-
-    # prepare request
-    while True:
-        # send request til server
-        client_socket.send(fil_name.encode())
-        message_from_server = client_socket.recv(1024)  # get the message from server
-        print("From server: ", message_from_server.decode())  # print it out
-        if message_from_server == "exit":
-            break
-    client_socket.close()
-
-
-if __name__ == '__main__':
-    """
-    server_host = sys.argv[1]  # first argument
-    server_port = int(sys.argv[2])  # second argument
-    filename = sys.argv[3]  # third argument"""
-
-    request_to_server("127.0.0.1", 2609, input("Write in filename: "))
+# prepare request
+while True:
+    # send request til server
+    fil_name = input("File name? ")
+    client_socket.send(fil_name.encode())
+    message_from_server = client_socket.recv(1024)  # get the message from server
+    print("From server: ", message_from_server.decode())  # print it out
+    if message_from_server == "exit":
+        break
+client_socket.close()
